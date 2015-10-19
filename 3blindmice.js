9d66bbb9e6d720019158931f5ef95b929ea540b5
schmitz1193 <schmitz1193@gmail.com>
2015-10-19 18:47:37 -0500

    delete buttons work...almost!

--- 3blindmice.js
+++ 3blindmice.js
@@ -3,19 +3,25 @@ var userMsg = document.getElementById("userMessage");
 
 var msgBox = document.getElementById("messageBox");
 
+var clearAll = document.getElementById("clear");
+
 var msgString = "";
 
-
+//checking for the enter in user message, if entered put value in variable and call function
 
 userMsg.addEventListener("keydown", function whenEnter (keypress) {
 	if (keypress.keyCode === 13) {
 		var message = userMsg.value;
 		stringMessage(message);
 
+//call function to create data string and button for the messages box
+
 		function stringMessage(message) {
-			msgString += "<div>" + message + "<button id='delete'>Delete</button></div>";
+			msgString += "<div>" + message + "<button class='delete'>Delete</button></div>";
 			msgBox.innerHTML = msgString;
 		}
+
+//call this function to clear the input in the enter message box
 
 		clearInput();
 
@@ -24,3 +30,38 @@ userMsg.addEventListener("keydown", function whenEnter (keypress) {
 		}
 	}
 });
+
+//check to see if the delete button in the messages box has been clicked.  if true, delete the string from the msgBox.innerHTML
+
+document.querySelector("body").addEventListener("click", function(event) {
+	if (event.target.className === "delete") {
+		console.log("you clicked on a delete button");
+	  // Delete div element including message and button
+	  console.log("parentNode ", event.target.parentNode)
+	   event.target.parentNode.remove();
+	}
+});
+
+// Clear all messages on the board
+
+clearAll.addEventListener("click", function() {
+	console.log("clearing input")
+	msgBox.innerHTML = "";
+	document.getElementById("checkboxes").checked = false;
+	// clear.setAttribute("disabled", true);
+});
+
+
+	// handle click events on checkboxes
+//	if (event.target.type === "checkbox") {
+//		// toggle darkTheme class on "messages-container element"
+//		if (event.target.id === "darkTheme") {
+//			document.getElementById("messages-container").classList.toggle("darkTheme");
+//	  	}
+//		// toggle largeText class on "messages-container element"
+//		else if (event.target.id === "largeText") {
+//			document.getElementById("messages-container").classList.toggle("largeText");
+//		}
+//	}
+//});
+
