f8fb2b0cd7b5b00df936ec87c9de1cfe689c9e47
schmitz1193 <schmitz1193@gmail.com>
2015-10-19 15:45:42 -0500

    multiple msgs in box with button

--- 3blindmice.js
+++ 3blindmice.js
@@ -1,15 +1,26 @@
+
 var userMsg = document.getElementById("userMessage");
 
 var msgBox = document.getElementById("messageBox");
 
 var msgString = "";
 
+
+
 userMsg.addEventListener("keydown", function whenEnter (keypress) {
 	if (keypress.keyCode === 13) {
-		msgString += "<div>" + userMsg.value + "</div>";
-		console.log("msgString ", msgString);
-		msgBox.innerHTML = msgString;
-		msgString = '';
+		var message = userMsg.value;
+		stringMessage(message);
+
+		function stringMessage(message) {
+			msgString += "<div>" + message + "<button id='delete'>Delete</button></div>";
+			msgBox.innerHTML = msgString;
+		}
+
+		clearInput();
+
+			function clearInput() {
+			userMsg.value = "";
+		}
 	}
 });
-
