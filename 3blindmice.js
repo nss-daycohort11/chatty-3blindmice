3662e0d470d6e7d8ced08c3061c7ae23d601a264
schmitz1193 <schmitz1193@gmail.com>
2015-10-19 13:58:38 -0500

    enter to msg box working

--- 3blindmice.js
+++ 3blindmice.js
@@ -1,1 +1,15 @@
-/// 3blindmice JavaScript
+var userMsg = document.getElementById("userMessage");
+
+var msgBox = document.getElementById("messageBox");
+
+var msgString = "";
+
+userMsg.addEventListener("keydown", function whenEnter (keypress) {
+	if (keypress.keyCode === 13) {
+		msgString += "<div>" + userMsg.value + "</div>";
+		console.log("msgString ", msgString);
+		msgBox.innerHTML = msgString;
+		msgString = '';
+	}
+});
+
