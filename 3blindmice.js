407c75e14d2f5ff402f75b8c7357faf248fd5cc2
schmitz1193 <schmitz1193@gmail.com>
2015-10-22 15:26:20 -0500

    Final JS changes

--- 3blindmice.js
+++ 3blindmice.js
@@ -1,76 +1,79 @@
-
-var userMsg = document.getElementById("userMessage");
-
-var msgBox = document.getElementById("messageBox");
-
-var clearAll = document.getElementById("clear");
-
-var msgString = "";
-
-//checking for the enter in user message, if entered put value in variable and call function
-
-userMsg.addEventListener("keydown", function whenEnter (keypress) {
-
-	if (keypress.keyCode === 13) {
-		clearAll.removeAttribute("disabled");
-		var message = userMsg.value;
-		stringMessage(message);
+$(document).ready(function() {
+  // All the JavaScript that depends on jQuery will be written here
 
 
+	var userMsg = $("#userMessage");
+console.log("userMsg.value", userMsg.value);
+	var msgBox = $("#messageBox");
 
-//call function to create data string and button for the messages box
+	var clearAll = $("#clear");
 
-		function stringMessage(message) {
-//			msgString += "<div id='entry'>" + message + "<button class='delete'>Delete</button></div>";
-//			msgBox.innerHTML = msgString;
-			var newElem = document.createElement('div');  //creates new div node
-            newElem.innerHTML = message + "<button class='delete'>Delete</button>";
-            newElem.setAttribute("id", "entry");
-            msgBox.appendChild(newElem);
+	var msgString = "";
+
+console.log("clearAll", clearAll);
+	//checking for the enter in user message, if entered put value in variable and call function
+
+	userMsg.keydown(function(keypress) {
+
+		if (keypress.keyCode === 13) {
+			clearAll.removeAttr("disabled");
+			var message = userMsg.val();
+			stringMessage(message);
+console.log("message", message);
+
+
+	//call function to create data string and button for the messages box
+
+			function stringMessage (message) {
+	//			msgString += "<div id='entry'>" + message + "<button class='delete'>Delete</button></div>";
+	//			msgBox.innerHTML = msgString;
+				var newElem = $("<div></div>");  //creates new div node
+	            newElem.html(message + "<button class='delete'>Delete</button>");
+	            newElem.attr("id", "entry");
+	            msgBox.append(newElem);
+			}
+
+	//call this function to clear the input in the enter message box
+
+			
+
+				
+				userMsg.val("");
+			
 		}
+	});
 
-//call this function to clear the input in the enter message box
+	//check to see if the delete button in the messages box has been clicked.  if true, delete the string from the msgBox.innerHTML
 
-		clearInput();
-
-			function clearInput() {
-			userMsg.value = "";
+	$("body").on("click",(".delete", function(event) {
+		if (event.target.className === "delete") {
+			console.log("you clicked on a delete button");
+		  // Delete div element including message and button
+		  console.log("parentNode ", event.target.parentNode)
+		   event.target.parentNode.remove();
 		}
-	}
+	}));
+
+	// Clear all messages on the board
+
+	clearAll.click (function() {
+		console.log("clearing input");
+		msgBox.html("");
+		clearAll.attr("checked");
+		clearAll.attr("disabled");
+	});
+
+	var allThings = $("#container");
+
+	// var messBoxBorder = document.getElementById("messageBox");  //doesn't work
+
+	    $("#dark").click (function() {   //do we have to have this function here
+	        allThings.classList.toggle("darken");
+	        //messBoxBorder.classList.toggle("lightBorder");  //this part doesn't work
+	        });
+
+	    $("#bigText").click (function() {
+	        allThings.toggleClass("giant");
+	        });
+		
 });
-
-//check to see if the delete button in the messages box has been clicked.  if true, delete the string from the msgBox.innerHTML
-
-document.querySelector("body").addEventListener("click", function(event) {
-	if (event.target.className === "delete") {
-		console.log("you clicked on a delete button");
-	  // Delete div element including message and button
-	  console.log("parentNode ", event.target.parentNode)
-	   event.target.parentNode.remove();
-	}
-});
-
-// Clear all messages on the board
-
-clearAll.addEventListener("click", function() {
-	console.log("clearing input");
-	msgBox.innerHTML = "";
-	document.getElementById("clear").checked = false;
-	clear.setAttribute("disabled", true);
-});
-
-var allThings = document.getElementById("container");
-
-// var messBoxBorder = document.getElementById("messageBox");  //doesn't work
-
-    document.getElementById("dark") 
-        .addEventListener("click", function() {   //do we have to have this function here
-        allThings.classList.toggle("darken");
-        //messBoxBorder.classList.toggle("lightBorder");  //this part doesn't work
-        });
-
-    document.getElementById("bigText")
-        .addEventListener("click", function() {
-        allThings.classList.toggle("giant");
-        });
-	
